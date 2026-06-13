963726f2af0812137b01e41aea2e2825c391deff
rahuljiara <114233457+rahuljiara@users.noreply.github.com>
2026-06-13 23:59:04 +0530

    Implementing searching functionality using regex to match string to search using options : i (to make string case insensitive) stored user searched query to variable to search using these data applied try-catch block for error handling

--- controller/productController.js
+++ controller/productController.js
@@ -1,20 +1,76 @@
 const ProductModel = require("../models/productModel")
 
 exports.getProduct = async (req, res, next) => {
-  // Geting all data from the database using model
-  const products = await ProductModel.find();
+  try {
 
-  res.status(200).json({ products });
+    /* Searching in database */
+
+    // extract all fields from query
+    const { brand, isFeatured, category, subCategory, color, name } = req.query;
+
+    // Blank object to store the key and value to search
+    const queryObject = {}
+
+    // all if will run if field matches and put data into empty object
+    if (brand) {
+      queryObject.brand = { $regex: brand, $options: 'i' };
+    }
+
+    if (category) {
+      queryObject.category = { $regex: category, $options: 'i' };
+    }
+
+    if (subCategory) {
+      queryObject.subCategory = { $regex: subCategory, $options: 'i' };
+    }
+
+    // by writing isFeatured it will considered true (==="true" makes boolean)
+    if (isFeatured) {
+      queryObject.isFeatured = isFeatured === "true";
+    }
+
+    if (color) {
+      queryObject["attributes.color"] = { $regex: color, $options: 'i' };
+      console.log(queryObject);
+    }
+
+    if (name) {
+      queryObject.name = { $regex: name, $options: 'i' };
+    }
+
+
+
+    // Geting all data from the database with matched query
+    const products = await ProductModel.find(queryObject);
+    res.status(200).json({ products });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
 
 exports.getProductTesting = async (req, res, next) => {
-  // Filtering method to get data base on key:value pair
-  // const products = await ProductModel.find({
-  //   brand: "Apple",
-  //   subCategory: "Smart Watches"
-  // });
+  try {
+    // Filtering method to get data base on key:value pair
 
-  // dynamic filtering method using query 
-  const products = await ProductModel.find(req.query);
-  res.status(200).json({ products });
+    /* 
+      const products = await ProductModel.find({
+      brand: "Apple",
+      subCategory: "Smart Watches"
+    });
+  
+    */
+    // dynamic filtering method using query 
+    const products = await ProductModel.find(req.query);
+    res.status(200).json({ products });
+
+  }
+  catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
